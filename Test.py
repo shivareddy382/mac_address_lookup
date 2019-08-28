838b3b5e704bb07b81986f5ec345a899d2af43cb
Shiva Reddy Maddi <shivareddymaddi@Shivas-MacBook-Air.local>
2019-08-27 22:12:24 -0500

    first commit

---
+++ Test.py
@@ -0,0 +1,12 @@
+import requests
+import json
+import os
+
+def get_mac_lookup():
+        mac_address = os.environ['macaddress']
+        print("You entered: " + mac_address)
+        print('https://api.macaddress.io/v1?apiKey=at_vPxEIQqhTXID0OPufpq1KdoZkjLNL&output=json&search={}'.format(mac_address))
+        response = requests.get('https://api.macaddress.io/v1?apiKey=at_vPxEIQqhTXID0OPufpq1KdoZkjLNL&output=json&search={}'.format(mac_address))
+        print(response.json()['vendorDetails']['companyName'])
+
+get_mac_lookup()
