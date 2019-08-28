392c2afc1852039da48a81f3855659a76b481e11
shivareddy382 <shivareddy382@gmail.com>
2019-08-27 22:38:14 -0500

    codechanges

--- Test.py
+++ Test.py
@@ -5,8 +5,8 @@ import os
 def get_mac_lookup():
         mac_address = os.environ['macaddress']
         print("You entered: " + mac_address)
-        print('https://api.macaddress.io/v1?apiKey=at_vPxEIQqhTXID0OPufpq1KdoZkjLNL&output=json&search={}'.format(mac_address))
+       #print('https://api.macaddress.io/v1?apiKey=at_vPxEIQqhTXID0OPufpq1KdoZkjLNL&output=json&search={}'.format(mac_address))
         response = requests.get('https://api.macaddress.io/v1?apiKey=at_vPxEIQqhTXID0OPufpq1KdoZkjLNL&output=json&search={}'.format(mac_address))
-        print(response.json()['vendorDetails']['companyName'])
-
+        print("Company Name: " + response.json()['vendorDetails']['companyName'])
+        
 get_mac_lookup()
